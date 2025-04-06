45668493e757ef843f9446a2d3a8172a9839df6d
lucas-hm <lucashugomoran12@gmail.com>
2025-04-06 19:57:16 +0000

    inicio de codigo

---
+++ src/roman_converter.py
@@ -0,0 +1,10 @@
+print("convertidor de numeros a numeros romanos")
+I = 1
+V = 5
+X = 10
+L = 50
+C = 100
+D = 500
+M = 1000
+
+numero = int(input("ingresa un numero y luego se convertira a romano desde otra carpeta: "))
